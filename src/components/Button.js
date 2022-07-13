3d0856da6ef8b8d3a0dd6ac9f11b80774789295a
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-13 16:35:39 -0700

    refractor to use classnames

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,19 +2,14 @@ import React from "react";
 
 import "components/Button.scss";
 import { action } from "@storybook/addon-actions";
+import classNames from "classnames";
 
 export default function Button(props) {
-   let buttonClass = "button";
+   const buttonClass = classNames('button',{
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   })
 
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   };
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-   };
-
- 
    return (
       <button 
          onClick={props.onClick} 
