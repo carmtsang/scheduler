5c5639d8b4b03180bdaa8955f8a1000a4e2202c0
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-13 16:00:26 -0700

    add danger, on click & disabled

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,28 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions";
 
 export default function Button(props) {
    let buttonClass = "button";
 
    if (props.confirm) {
       buttonClass += " button--confirm";
-   }
+   };
 
-   return <button className={buttonClass}>{props.children}</button>;
+   if (props.danger) {
+      buttonClass += " button--danger";
+   };
+
+ 
+   return (
+      <button 
+         onClick={props.onClick} 
+         disabled={props.disabled} 
+         className={buttonClass}
+      >
+         {props.children}
+      </button>
+   );
+
 }
