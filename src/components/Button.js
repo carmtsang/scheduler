a8e7e078cd71036a61817752239c66ea51304a7b
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-13 15:29:49 -0700

    add button class, return button props.children

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,11 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+
+   if (props.confirm) {
+      buttonClass += " button--confirm";
+   }
+
+   return <button className={buttonClass}>{props.children}</button>;
 }
