aba672b05381e92dbb79c9bb6baf05b024d255af
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-14 15:49:29 -0700

    add dummy data, set state & render onto app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
+import DayList from 'components/DayList';
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,6 +33,11 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
+        <DayList
+          days={days}
+          day={day}
+          setDay={setDay}
+        />
         <nav className="sidebar__menu"></nav>
         <img
           className="sidebar__lhl sidebar--centered"
