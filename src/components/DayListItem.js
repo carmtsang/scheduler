cb17eaca7fe8ecfc1dd17126b71bbfff6b78250f
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-13 20:44:16 -0700

    func formatSpots for wording depending on days

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,13 +6,25 @@ export default function DayListItem(props) {
   const dayListClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
-  })
+  });
+
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return 'no spots remaining'
+    }
+    if (spots === 1) {
+      return `${spots} spot remaining`
+    }
+    if (props.spots > 1) {
+      return `${spots} spots remaining`
+    }
+  };
 
   return (
     <li onClick={() => props.setDay(props.name)}
       className={dayListClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
