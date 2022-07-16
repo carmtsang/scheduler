2fab06f66edeff94c66034134603c6ca2c0a5b56
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-16 14:36:14 -0700

    move formatSports out of func.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,29 +2,31 @@ import React from 'react';
 import "components/DayListItem.scss"
 import classNames from 'classnames';
 
+const formatSpots = (spots) => {
+  if (spots === 0) {
+    return 'no spots remaining'
+  }
+  if (spots === 1) {
+    return `${spots} spot remaining`
+  }
+  if (spots > 1) {
+    return `${spots} spots remaining`
+  }
+};
+
 export default function DayListItem(props) {
   const dayListClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   });
 
-  const formatSpots = (spots) => {
-    if (spots === 0) {
-      return 'no spots remaining'
-    }
-    if (spots === 1) {
-      return `${spots} spot remaining`
-    }
-    if (props.spots > 1) {
-      return `${spots} spots remaining`
-    }
-  };
+  let remainingSpots = formatSpots(props.spots)
 
   return (
     <li onClick={() => props.setDay(props.name)}
       className={dayListClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+      <h3 className="text--light">{remainingSpots}</h3>
     </li>
   );
 }
