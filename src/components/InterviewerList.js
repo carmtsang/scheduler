5a97e1ec41ffb62538be3c452a3620a95e77a028
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-16 16:16:48 -0700

    render list of interviewers

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import "components/InterviewerList.scss"
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList(props) {
+  const interviewersList = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem 
+        {...interviewer}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+      />
+    )
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewersList}
+      </ul>
+    </section>
+  )
+}
