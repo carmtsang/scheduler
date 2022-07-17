7bff38175a6349adf6f8f531d3cf615e5f908ad9
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-17 12:01:57 -0700

    update clickable event for Interviewerlistitem

--- stories/index.js
+++ stories/index.js
@@ -97,12 +97,12 @@ storiesOf("InterviewerListItem", module)
   ))
   .add("Clickable", () => (
     <InterviewerListItem
-      id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={() => action("setInterviewer")(interviewer.id)}
     />
   ));
+  
 
   const interviewers = [
     { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
