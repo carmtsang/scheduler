7aac52285451f55bd216397573b04ecb7d2eb891
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-18 15:34:41 -0700

    refractor to use props

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -5,10 +5,10 @@ import Button from 'components/Button';
 export default function Confirm(props) {
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <h1 className="text--semi-bold">{props.message}</h1>
       <section className="appointment__actions">
-        <Button danger>Cancel</Button>
-        <Button danger>Confirm</Button>
+        <Button danger onClick={props.onCancel}>Cancel</Button>
+        <Button danger onClick={props.onConfirm}>Confirm</Button>
       </section>
     </main>
   );
