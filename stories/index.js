c40d78f7ee4feb951d90789d566e526ba0e37728
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-18 16:42:22 -0700

    add Edit & Create for Form on Appointment

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
 import Error from 'components/Appointment/Error';
+import Form from 'components/Appointment/Form';
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,3 +153,5 @@ storiesOf("Appointment", module)
   .add('Confirm', () => <Confirm message={'Delete the appointment?'} onConfirm={action('onConfirm')} onCancel={action('onCancel')} />)
   .add('Status', () => <Status message={'Deleting'} />)
   .add('Error', () => <Error message={'Could not delete Appointment'} onClose={action('onClose')} />)
+  .add('Edit', () => <Form student={"Carmen Tsang"} interviewer={3} interviewers={interviewers} onSave={action('onSave')} onCancel={action('onCancel')} />)
+  .add('Create', () => <Form interviewers={interviewers} onSave={action('onSave')} onCancel={action('onCancel')} />)
