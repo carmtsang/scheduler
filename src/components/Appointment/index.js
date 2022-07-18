5985a49b82546fbe76d9c09f1573094602424a71
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-18 15:45:51 -0700

    refractor to use props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './Header';
+
+import 'components/Appointment/styles.scss'
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+    </article>
   );
 };
