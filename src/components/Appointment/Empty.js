9c2dc7020db2d31ed2141e5b63f71b851db89532
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-17 18:05:59 -0700

    refractor to accept props

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -7,6 +7,7 @@ export default function Empty(props) {
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
+        onClick={props.onAdd}
       />
     </main>
   );
