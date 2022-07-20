f73b4832721efa2b8f15f18c65acd350fb57507d
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-19 18:20:10 -0700

    debug prevent default

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -21,7 +21,7 @@ export default function Form(props) {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={e => e.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -35,7 +35,6 @@ export default function Form(props) {
           interviewers={interviewers}
           value={interviewer}
           onChange={setInterviewer}
-          onSubmit={e => e.preventDefault()}
         />
       </section>
       <section className="appointment__card-right">
