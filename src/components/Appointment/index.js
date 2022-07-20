cf55da1aedcb2837d84ab9a0ac79e56fd141a460
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-19 17:04:05 -0700

    import Show, Empty & render

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import Header from './Header';
-
+import React, { Fragment } from 'react';
 import 'components/Appointment/styles.scss'
 
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+
 export default function Appointment(props) {
+  const  { time, interview } = props;
+
   return (
     <article className="appointment">
-      <Header time={props.time}/>
+      <Header time={time} />
+      <Fragment>
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+      </Fragment>
     </article>
   );
 };
