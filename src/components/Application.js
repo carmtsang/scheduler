1ab6ef21f71534a5e4da8bc6b0774c8479a535fc
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-22 15:00:54 -0700

    set state of days/app using promise. helper func to display

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,21 +11,25 @@ export default function Application(props) {
     day: "Monday",
     days: [],
     appointments: {}
-  })
-  const setDay = day => setState({...state, day });
-  const setDays = days => setState(prev => ({...prev, days }));
+  });
 
-  const dailyAppointments = [];
+  const setDay = day => setState({...state, day });
+  let dailyAppointments = [];
 
   useEffect(() => {
-    const dayURL = `http://localhost:8001/api/days`
-    axios.get(dayURL)
-      .then(res =>
-        setDays(res.data)
-      );
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
+    ]).then(all => {
+      // set state after retrieving api.
+      setState(prev =>({...prev, days: all[0].data, appointments: all[1].data }))
+    })
   }, []);
 
-  const appintmentComponent = dailyAppointments.map(appointment => {
+  dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const appointmentComponent = dailyAppointments.map(appointment => {
     return (<Appointment 
       key={appointment.id}
       {...appointment} />
@@ -54,7 +58,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-          {appintmentComponent}
+          {appointmentComponent}
           <Appointment key="last" time="5pm" />
       </section>
     </main>
