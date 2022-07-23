a7e8ace8e06eeb7a9e282eec69c90a7499cc9313
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-23 15:29:58 -0700

    add onCancel w/ Transition, add onDelete, onEdit

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,27 +5,37 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 import useVisualMode from 'hooks/useVisualMode';
+import Form from './Form';
 
 const EMPTY = 'EMPTY';
 const SHOW = 'SHOW';
+const CREATE = 'CREATE';
 
 export default function Appointment(props) {
-  const  { time, interview } = props;
+  const  { time, interview, interviewers } = props;
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
   
+
   return (
     <article className="appointment">
       <Header time={time} />
       <Fragment>
-        {mode === EMPTY && <Empty onAdd={() => console.log('Clicked onAdd')} />}
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
         {mode === SHOW && (
-          <Show
-            student={interview.student}
-            interviewer={interview.interviewer}
-          />
-        )}
+          <Show student={interview.student} 
+          interviewer={interview.interviewer}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          />)
+        }
+        {mode === CREATE && (
+        <Form 
+          onSave={() => {}}
+          interviewers={interviewers}
+          onCancel={() => transition(EMPTY)}
+        />)}
       </Fragment>
     </article>
   );
