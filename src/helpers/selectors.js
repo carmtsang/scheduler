e901b9f2d7f14ff2de68e4e46f756a6b23914a82
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-22 17:49:42 -0700

    implement getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-// loop through state, if it matches the day, for each of the appointments, if the appointment obj matches, push it to the results array
+// loop through state.days, if it matches the day, for each of the appointments, if the appointment obj matches, push it to the results array
 
 export function getAppointmentsForDay(state, day) {
   let results = [];
@@ -9,4 +9,18 @@ export function getAppointmentsForDay(state, day) {
   };
 
   return results;
+};
+
+// loop through state.interviewers, if match, return interview obj with student name & interviewer obj
+export function getInterview(state, interview) {
+  if (interview === null) return null;
+
+  for (let person in state.interviewers) {
+    if (state.interviewers[person].id === interview.interviewer) {
+      return {
+        student: interview.student,
+        interviewer: state.interviewers[person]
+      }
+    }
+  };
 };
