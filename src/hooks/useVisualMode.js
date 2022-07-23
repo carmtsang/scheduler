1edc385faeb40797e5c3139ba5d8db5be53897df
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-23 13:53:07 -0700

    add replace to transition func

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = mode => {
+  // if replace is false, add the mode to history, if true we don't add mode to history
+  const transition = (mode, replace = false) => {
     setMode(mode);
-    setHistory(prev => [...prev, mode]);
+    
+    if (!replace) {
+      setHistory(prev => [...prev, mode]);
+    }
   };
-
+  
+  // go back by setting mode to previous one in history and remove the last item in history array.
   const back = () => {
     if (history.length > 1) {
       setMode(history[history.length - 2]);
