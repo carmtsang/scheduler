157f4b358a6baf9b65aeff37fbd4114064fd338b
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-23 14:32:42 -0700

    import useVisualMode, add logic to interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,10 +4,17 @@ import 'components/Appointment/styles.scss'
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import useVisualMode from 'hooks/useVisualMode';
+
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
 
 export default function Appointment(props) {
   const  { time, interview } = props;
-
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+  
   return (
     <article className="appointment">
       <Header time={time} />
