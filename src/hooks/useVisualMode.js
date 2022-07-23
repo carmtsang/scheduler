e039d97372ca3499eec92f48af76bdbb2553487e
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-23 12:48:28 -0700

    add history state & back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(mode) {
-    const newMode = setMode(mode);
-    return newMode;
-  }
+  const transition = mode => {
+    setMode(mode);
+    setHistory(prev => [...prev, mode]);
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      setHistory(prev => [...prev.slice(0, -1)]);
+    } 
+  };
   
-  return { mode, transition };
+  return { mode, transition, back };
 }
