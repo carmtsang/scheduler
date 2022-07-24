fa3227b63666417e5e1e094a8194e8fb931003d3
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-24 12:36:37 -0700

    debug, move save to appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -12,11 +12,19 @@ const SHOW = 'SHOW';
 const CREATE = 'CREATE';
 
 export default function Appointment(props) {
-  const  { time, interview, interviewers } = props;
+  const  { id, time, interview, interviewers, bookInterview } = props;
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
   
+  const save = (name, interviewer) => {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    bookInterview(id, interview);
+  };
+
   return (
     <article className="appointment">
       <Header time={time} />
@@ -31,7 +39,7 @@ export default function Appointment(props) {
         }
         {mode === CREATE && (
         <Form 
-          onSave={() => {}}
+          onSave={save}
           interviewers={interviewers}
           onCancel={() => back()}
         />)}
