038354a732352a7b12435c1ceba5976dfb7d08b2
carmtsang <carmen.tsang1208@hotmail.com>
2022-07-24 16:37:30 -0700

    add SAVING mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,23 +6,26 @@ import Show from './Show';
 import Empty from './Empty';
 import useVisualMode from 'hooks/useVisualMode';
 import Form from './Form';
+import Status from './Status'
 
 const EMPTY = 'EMPTY';
 const SHOW = 'SHOW';
 const CREATE = 'CREATE';
+const SAVING = 'SAVING'
 
 export default function Appointment(props) {
   const  { id, time, interview, interviewers, bookInterview } = props;
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
-  
+
   const save = (name, interviewer) => {
     // creates an interview obj from arguments in onSave
     const interview = {
       student: name,
       interviewer
     };
+
     bookInterview(id, interview);
 
     transition(SHOW);
@@ -41,11 +44,17 @@ export default function Appointment(props) {
           />)
         }
         {mode === CREATE && (
-        <Form 
-          onSave={save}
-          interviewers={interviewers}
-          onCancel={() => back()}
-        />)}
+          <Form 
+            onSave={save}
+            interviewers={interviewers}
+            onCancel={() => back()}
+          />)
+        }
+        {mode === SAVING && (
+          <Status
+            message={'Saving Interview'}
+          />)
+        }
       </Fragment>
     </article>
   );
