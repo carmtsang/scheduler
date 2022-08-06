a00e4e5a5e0704fa7a51fddf5c115e6fa60c03ce
carmtsang <carmen.tsang1208@hotmail.com>
2022-08-06 16:22:34 -0700

    update onclick to validate

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -57,7 +57,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => validate()}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
